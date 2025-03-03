2a8081aedfb806928dad5c8180d22f12ae9a53c1
ZSHenChan <zishenchan@gmail.com>
2025-03-03 19:25:35 +0800

    change port

--- src/app/lib/grpc/xcuisite.Client/grpcClient.js
+++ src/app/lib/grpc/xcuisite.Client/grpcClient.js
@@ -4,19 +4,21 @@ import { GetProductsServiceClient } from "@/lib/proto/products_grpc_web_pb";
 // import { grpc } from "@improbable-eng/grpc-web";
 
 // envoy listens on 8080
-const BASE_URL = "http://localhost:";
+// const BACKEND_BASE_URL = "http://localhost:";
+// const PORT = "8080";
+const BACKEND_BASE_URL = "http://20.212.41.187:";
 const PORT = "8080";
 
 const orderServiceClient = new OrderServiceClient(
-  `${BASE_URL}${PORT}`,
+  `${BACKEND_BASE_URL}${PORT}`,
   null,
   {}
 );
 
-const pricingClient = new PricingClient(`${BASE_URL}${PORT}`, null, {});
+const pricingClient = new PricingClient(`${BACKEND_BASE_URL}${PORT}`, null, {});
 
 const getProductsClient = new GetProductsServiceClient(
-  `${BASE_URL}${PORT}`,
+  `${BACKEND_BASE_URL}${PORT}`,
   null,
   {}
 );
